6bed9f70a7e45e46a5fc8dd1dc9e76b31a8a868a
Theodore DeRego <tderego94@gmail.com>
2015-06-05 16:00:02 -1000

    Game controller support

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,11 @@ use std::net::UdpSocket;
 use std::sync::mpsc::channel;
 use std::thread;
 
+use sdl2::controller::{
+    Axis,
+    GameController,
+};
+
 extern crate sdl2;
 extern crate piston;
 extern crate conrod;
@@ -31,6 +36,7 @@ use conrod::color::{rgb, white};
 use graphics::Context;
 use opengl_graphics::{GlGraphics, OpenGL};
 use opengl_graphics::glyph_cache::GlyphCache;
+use piston::input;
 use piston::event::*;
 use piston::window::{WindowSettings, Size};
 use sdl2_window::Sdl2Window;
@@ -40,6 +46,9 @@ struct RoverUi {
     bg_color: Color,
     l_rpm: f32,
     r_rpm: f32,
+    both_rpm: bool,
+    
+    max_rpm: f32,
     
     l_rpm_status: String,
     r_rpm_status: String,
@@ -53,9 +62,31 @@ impl RoverUi {
             bg_color: rgb(0.2, 0.35, 0.45),
             l_rpm: 0.0,
             r_rpm: 0.0,
+            both_rpm: false,
+            
+            max_rpm: 2000.0,
+            
             l_rpm_status: "UNAVAILABLE".to_string(),
             r_rpm_status: "UNAVAILABLE".to_string(),
             socket: socket,
+        }
+    }
+    
+    fn on_key_pressed(&mut self, key: input::Key) {
+        match key {
+            input::Key::RCtrl | input::Key::LCtrl => {
+                self.both_rpm = true;
+            },
+            _ => { },
+        }
+    }
+    
+    fn on_key_released(&mut self, key: input::Key) {
+        match key {
+            input::Key::RCtrl | input::Key::LCtrl => {
+                self.both_rpm = false;
+            },
+            _ => { },
         }
     }
 }
@@ -79,7 +110,7 @@ fn main() {
     let mut ui = Ui::new(glyph_cache, theme);
     
     // Initialize game pad
-    //init_game_pad();
+    let controller = init_game_pad();
     
     // Create a UDP socket to talk to the rover
     let socket = UdpSocket::bind("0.0.0.0:30001").unwrap();
@@ -103,6 +134,20 @@ fn main() {
     for e in event_iter {
         ui.handle_event(&e);
         
+        e.press(|button| {
+            match button {
+                input::Button::Keyboard(key) => rover_ui.on_key_pressed(key), 
+                _ => { },
+            }
+        });
+        
+        e.release(|button| {
+            match button {
+                input::Button::Keyboard(key) => rover_ui.on_key_released(key), 
+                _ => { },
+            }
+        });
+        
         // Update
         e.update(|_| {
             if let Ok(msg) = packet_r.try_recv() {
@@ -110,6 +155,22 @@ fn main() {
                 let rpm_parts: Vec<String> = msg.split(":").map(|s| s.to_string()).collect();
                 rover_ui.l_rpm_status = rpm_parts[0].clone();
                 rover_ui.r_rpm_status = rpm_parts[1].clone();
+            }
+            
+            if let Some(ref controller) = controller {
+                let left_y = controller.get_axis(Axis::LeftY).unwrap();
+                let right_y = controller.get_axis(Axis::RightY).unwrap();
+                
+                let l_rpm = -(left_y as f32 / 32768.0) * rover_ui.max_rpm;
+                let r_rpm = -(right_y as f32 / 32768.0) * rover_ui.max_rpm;
+                
+                if (l_rpm - rover_ui.l_rpm).abs() > 5.0 || (r_rpm - rover_ui.r_rpm).abs() > 5.0 {
+                    rover_ui.l_rpm = l_rpm;
+                    rover_ui.r_rpm = r_rpm;
+                    
+                    let rpm_packet = format!("{}:{}", rover_ui.l_rpm as i32, rover_ui.r_rpm as i32);
+                    rover_ui.socket.send_to(rpm_packet.as_bytes(), ("10.10.153.25", 30001)).unwrap();
+                }
             }
         });
         
@@ -128,7 +189,13 @@ fn draw_ui<'a>(c: Context, gl: &mut GlGraphics, ui: &mut Ui<GlyphCache<'a>>, rov
     Background::new().color(rover_ui.bg_color).draw(ui, gl);
 
     // Left RPM slider
-    Slider::new(rover_ui.l_rpm, -1000.0, 1000.0)
+    let l_rpm =
+        if rover_ui.both_rpm {
+            rover_ui.l_rpm.max(rover_ui.r_rpm)
+        } else {
+            rover_ui.l_rpm
+        };
+    Slider::new(l_rpm, -rover_ui.max_rpm, rover_ui.max_rpm)
         .dimensions(200.0, 30.0)
         .xy(110.0 - (ui.win_w / 2.0), (ui.win_h / 2.0) - 25.0)
         .rgb(0.5, 0.3, 0.6)
@@ -137,6 +204,9 @@ fn draw_ui<'a>(c: Context, gl: &mut GlGraphics, ui: &mut Ui<GlyphCache<'a>>, rov
         .label_color(white())
         .react(|new_rpm| {
             rover_ui.l_rpm = new_rpm;
+            if rover_ui.both_rpm {
+                rover_ui.r_rpm = new_rpm;
+            }
             
             let rpm_packet = format!("{}:{}", rover_ui.l_rpm as i32, rover_ui.r_rpm as i32);
             rover_ui.socket.send_to(rpm_packet.as_bytes(), ("10.10.153.25", 30001)).unwrap();
@@ -144,7 +214,13 @@ fn draw_ui<'a>(c: Context, gl: &mut GlGraphics, ui: &mut Ui<GlyphCache<'a>>, rov
         .set(L_RPM_SLIDER, ui);
     
     // Right RPM slider
-    Slider::new(rover_ui.r_rpm, -1000.0, 1000.0)
+    let r_rpm =
+        if rover_ui.both_rpm {
+            rover_ui.l_rpm.max(rover_ui.r_rpm)
+        } else {
+            rover_ui.r_rpm
+        };
+    Slider::new(r_rpm, -rover_ui.max_rpm, rover_ui.max_rpm)
         .dimensions(200.0, 30.0)
         .xy((ui.win_w / 2.0) - 110.0, (ui.win_h / 2.0) - 25.0)
         .rgb(0.5, 0.3, 0.6)
@@ -153,6 +229,9 @@ fn draw_ui<'a>(c: Context, gl: &mut GlGraphics, ui: &mut Ui<GlyphCache<'a>>, rov
         .label_color(white())
         .react(|new_rpm| {
             rover_ui.r_rpm = new_rpm;
+            if rover_ui.both_rpm {
+                rover_ui.l_rpm = new_rpm;
+            }
             
             let rpm_packet = format!("{}:{}", rover_ui.l_rpm as i32, rover_ui.r_rpm as i32);
             rover_ui.socket.send_to(rpm_packet.as_bytes(), ("10.10.153.25", 30001)).unwrap();
@@ -197,9 +276,10 @@ fn draw_ui<'a>(c: Context, gl: &mut GlGraphics, ui: &mut Ui<GlyphCache<'a>>, rov
     //rover_ui.socket.send_to(rpm_packet.as_bytes(), ("192.168.240.1", 30001)).unwrap();
 }
 
-pub fn init_game_pad() {
+pub fn init_game_pad() -> Option<GameController> {
     use sdl2::{joystick, controller};
-    use sdl2::controller::GameController;
+    
+    println!("Looking for game controller...");
 
     let available =
         match joystick::num_joysticks() {
@@ -209,23 +289,20 @@ pub fn init_game_pad() {
 
     println!("{} joysticks available", available);
 
-    let mut controller = None;
-
     // Iterate over all available joysticks and look for game
     // controllers.
     for id in 0..available {
         if controller::is_game_controller(id) {
-            println!("Attempting to open controller {}", id);
+            println!("Attempting to open game controller {}", id);
 
             match GameController::open(id) {
                 Ok(c) => {
                     // We managed to find and open a game controller,
                     // exit the loop
                     println!("Success: opened \"{}\"", c.name());
-                    controller = Some(c);
-                    break;
+                    return Some(c);
                 },
-                Err(e) => println!("failed: {:?}", e),
+                Err(e) => println!("Failed to open game controller: {:?}", e),
             }
 
         } else {
@@ -233,13 +310,7 @@ pub fn init_game_pad() {
         }
     }
 
-    let controller =
-        match controller {
-            Some(c) => c,
-            None     => panic!("Couldn't open any controller"),
-        };
-
-    println!("Controller mapping: {}", controller.mapping());
+    None
 }
 
 // Widget IDs
