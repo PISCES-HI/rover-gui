2e04b50639a3095b16e0618977b5a3eb846f7067
Theodore DeRego <tderego94@gmail.com>
2015-07-22 16:41:48 -1000

    blade gui

---
+++ src/blade.rs
@@ -0,0 +1,174 @@
+#![feature(convert)]
+use std::net::UdpSocket;
+use std::path::Path;
+use std::sync::{Arc, Mutex};
+use std::sync::mpsc::channel;
+use std::thread;
+
+extern crate time;
+extern crate sdl2;
+extern crate piston;
+extern crate conrod;
+extern crate graphics;
+extern crate opengl_graphics;
+extern crate sdl2_window;
+extern crate ffmpeg;
+extern crate image;
+
+use conrod::{
+    Theme,
+    Ui,
+};
+use opengl_graphics::{GlGraphics, OpenGL, Texture};
+use opengl_graphics::glyph_cache::GlyphCache;
+use piston::input;
+use piston::event::*;
+use piston::window::{WindowSettings, Size};
+use sdl2::controller;
+use sdl2_window::Sdl2Window;
+
+use blade_ui::BladeUi;
+use video_stream::{init_ffmpeg, start_video_stream};
+
+pub mod line_graph;
+pub mod blade_ui;
+pub mod video_stream;
+
+fn main() {
+    init_ffmpeg();
+
+    let opengl = OpenGL::_3_2;
+    let window = Sdl2Window::new(
+        WindowSettings::new(
+            "PISCES Navigation".to_string(),
+            Size { width: 1280, height: 700 }
+        )
+        .exit_on_esc(true)
+        .samples(4)
+    );
+    let event_iter = window.events().ups(20).max_fps(60);
+    let mut gl = GlGraphics::new(opengl);
+
+    let font_path = Path::new("./assets/fonts/NotoSans-Regular.ttf");
+    let theme = Theme::default();
+    let glyph_cache = GlyphCache::new(&font_path).unwrap();
+    let mut ui = Ui::new(glyph_cache, theme);
+    
+    // Initialize game pad
+    let controller = init_game_controller();
+    
+    // Create a UDP socket to talk to the rover
+    let socket = UdpSocket::bind("0.0.0.0:30001").unwrap();
+    socket.send_to(b"connect me plz", ("10.10.153.25", 30001));
+    
+    let in_socket = socket.try_clone().unwrap();
+    let (packet_t, packet_r) = channel();
+    
+    thread::Builder::new()
+        .name("packet_in".to_string())
+        .spawn(move || {
+            let mut buf = [0u8; 64];
+            loop {
+                let (bytes_read, _) = in_socket.recv_from(&mut buf).unwrap();
+                if let Ok(msg) = String::from_utf8(buf[0..bytes_read].iter().cloned().collect()) {
+                    packet_t.send(msg).unwrap();
+                }
+            }
+        }).unwrap();
+    
+    let mut blade_ui = BladeUi::new(socket);
+    
+    ////////////////////////////////////////////////////////////////////////////////////////
+    
+    let (mut video_texture, video_image) = start_video_stream("rtsp://root:pisces@10.10.153.28/axis-media/media.amp");
+    
+    ///////////////////////////////////////////////////////////////////////////////////////
+
+    for e in event_iter {
+        ui.handle_event(&e);
+        
+        e.press(|button| {
+            match button {
+                input::Button::Keyboard(key) => blade_ui.on_key_pressed(key), 
+                _ => { },
+            }
+        });
+        
+        e.release(|button| {
+            match button {
+                input::Button::Keyboard(key) => blade_ui.on_key_released(key), 
+                _ => { },
+            }
+        });
+        
+        // Update
+        e.update(|_| {
+            while let Ok(packet) = packet_r.try_recv() {
+                blade_ui.handle_packet(packet);
+            }
+            
+            if let Some(ref controller) = controller {
+                // Control RPM with analog sticks
+                let left_y = controller.get_axis(controller::Axis::LeftY);
+                let blade = -(left_y as f32 / 32768.0) * 100.0;
+
+                blade_ui.try_update_blade(blade);
+            }
+            
+            let video_image = video_image.lock().unwrap();
+            video_texture.update(&*video_image);
+        });
+        
+        // Render GUI
+        e.render(|args| {
+            gl.draw(args.viewport(), |c, gl| {
+                use graphics::*;
+            
+                blade_ui.draw_ui(c, gl, &mut ui);
+                
+                Rectangle::new([0.0, 0.0, 0.4, 1.0])
+                    .draw([1280.0 - 700.0 - 5.0, 5.0, 700.0, 400.0],
+                          &c.draw_state, c.transform,
+                          gl);
+                image(&video_texture, c.trans(1280.0 - 700.0 - 5.0, 5.0).scale(700.0/512.0, 400.0/512.0).transform, gl);
+            });
+        });
+    }
+}
+
+pub fn init_game_controller() -> Option<controller::GameController> {
+    use sdl2::{joystick, controller};
+    
+    println!("Looking for game controller...");
+
+    let available =
+        match joystick::num_joysticks() {
+            Ok(n)  => n,
+            Err(e) => panic!("can't enumerate joysticks: {}", e),
+        };
+
+    println!("{} joysticks available", available);
+
+    // Iterate over all available joysticks and look for game
+    // controllers.
+    for id in 0..available {
+        if controller::is_game_controller(id) {
+            println!("Attempting to open game controller {}", id);
+
+            match controller::GameController::open(id) {
+                Ok(c) => {
+                    // We managed to find and open a game controller,
+                    // exit the loop
+                    println!("Success: opened \"{}\"", c.name());
+                    return Some(c);
+                },
+                Err(e) => println!("Failed to open game controller: {:?}", e),
+            }
+
+        } else {
+             println!("{} is not a game controller", id);
+        }
+    }
+
+    None
+}
---
+++ src/blade_ui.rs
@@ -0,0 +1,286 @@
+use std::io;
+use std::net::UdpSocket;
+use std::ops::DerefMut;
+
+use conrod::{
+    Background,
+    Button,
+    Color,
+    Colorable,
+    Frameable,
+    Label,
+    Labelable,
+    Positionable,
+    Slider,
+    Sizeable,
+    Ui,
+    WidgetId,
+    Widget,
+};
+use conrod::color::{rgb, white};
+use graphics::Context;
+use opengl_graphics::GlGraphics;
+use opengl_graphics::glyph_cache::GlyphCache;
+use piston::input;
+use time;
+
+enum MissionTime {
+    Paused(time::Duration),
+    Running(time::Tm, time::Duration),
+}
+
+pub struct BladeUi {
+    bg_color: Color,
+    
+    mission_time: MissionTime,
+    
+    pub blade: f32,
+    
+    socket: UdpSocket,
+}
+
+impl BladeUi {
+    pub fn new(socket: UdpSocket) -> BladeUi {
+        BladeUi {
+            bg_color: rgb(0.2, 0.35, 0.45),
+            
+            mission_time: MissionTime::Paused(time::Duration::zero()),
+            
+            blade: 0.0,
+            
+            socket: socket,
+        }
+    }
+    
+    pub fn draw_ui<'a>(&mut self, c: Context, gl: &mut GlGraphics, ui: &mut Ui<GlyphCache<'a>>) {
+        use graphics::*;
+    
+        // Draw the background.
+        Background::new().color(self.bg_color).draw(ui, gl);
+        
+        let time_now = time::now();
+        
+        // Local time
+        Label::new(format!("{}", time_now.strftime("Local  %x  %X").unwrap()).as_str())
+            .xy((-ui.win_w / 2.0) + 100.0, (ui.win_h / 2.0) - 10.0)
+            .font_size(16)
+            .color(self.bg_color.plain_contrast())
+            .set(LOCAL_TIME, ui);
+        
+        // UTC time
+        Label::new(format!("{}", time_now.to_utc().strftime("%Z  %x  %X").unwrap()).as_str())
+            .xy((-ui.win_w / 2.0) + 104.0, (ui.win_h / 2.0) - 30.0)
+            .font_size(16)
+            .color(self.bg_color.plain_contrast())
+            .set(UTC_TIME, ui);
+        
+        // Mission time label
+        let mission_time =
+            match self.mission_time {
+                MissionTime::Paused(t) => t,
+                MissionTime::Running(start_time, extra_time) =>
+                    (time::now() - start_time) + extra_time
+            };
+        let total_days = mission_time.num_days();
+        let total_hours = mission_time.num_hours();
+        let total_minutes = mission_time.num_minutes();
+        let total_seconds = mission_time.num_seconds();
+        
+        let days = total_days;
+        let hours = total_hours - total_days*24;
+        let minutes = total_minutes - total_hours*60;
+        let seconds = total_seconds - total_minutes*60;
+        Label::new(format!("Mission Time: {}:{}:{}:{}", days, hours, minutes, seconds).as_str())
+            .xy((-ui.win_w / 2.0) + 150.0, (ui.win_h / 2.0) - 70.0)
+            .font_size(20)
+            .color(self.bg_color.plain_contrast())
+            .set(MISSION_TIME_LABEL, ui);
+        
+        // Mission start/pause button
+        let mission_start_text =
+            match self.mission_time {
+                MissionTime::Paused(_) => "Start",
+                MissionTime::Running(_, _) => "Pause",
+            };
+        Button::new()
+            .dimensions(100.0, 30.0)
+            .xy((-ui.win_w / 2.0) + 55.0, (ui.win_h / 2.0) - 100.0)
+            .rgb(0.3, 0.8, 0.3)
+            .frame(1.0)
+            .label(mission_start_text)
+            .react(|| {
+                match self.mission_time {
+                    MissionTime::Paused(current_time) => {
+                        self.mission_time = MissionTime::Running(time::now(), current_time);
+                    },
+                    MissionTime::Running(start_time, extra_time) => {
+                        self.mission_time = MissionTime::Paused((time::now() - start_time) + extra_time);
+                    },
+                };
+            })
+            .set(MISSION_START_BUTTON, ui);
+        
+        // Mission reset button
+        Button::new()
+            .dimensions(100.0, 30.0)
+            .xy((-ui.win_w / 2.0) + 160.0, (ui.win_h / 2.0) - 100.0)
+            .rgb(0.3, 0.8, 0.3)
+            .frame(1.0)
+            .label("Reset")
+            .react(|| {
+                self.mission_time = MissionTime::Paused(time::Duration::zero());
+            })
+            .set(MISSION_RESET_BUTTON, ui);
+        
+        // Time delay
+        Label::new("Time Delay: 0s")
+            .xy((-ui.win_w / 2.0) + 70.0, (ui.win_h / 2.0) - 150.0)
+            .font_size(18)
+            .color(self.bg_color.plain_contrast())
+            .set(TIME_DELAY, ui);
+        
+        // IMU label
+        Label::new("IMU")
+            .xy((-ui.win_w / 2.0) + 100.0, (ui.win_h / 2.0) - 190.0)
+            .font_size(22)
+            .color(self.bg_color.plain_contrast())
+            .set(IMU_LABEL, ui);
+        
+        // GPS label
+        Label::new("GPS")
+            .xy((-ui.win_w / 2.0) + 50.0, (ui.win_h / 2.0) - 400.0)
+            .font_size(22)
+            .color(self.bg_color.plain_contrast())
+            .set(GPS_LABEL, ui);
+        
+        // Longitude label
+        Label::new("19 43' 1\" N")
+            .xy((-ui.win_w / 2.0) + 50.0, (ui.win_h / 2.0) - 425.0)
+            .font_size(16)
+            .color(self.bg_color.plain_contrast())
+            .set(LONGITUDE_LABEL, ui);
+        
+        // Latitude label
+        Label::new("155 4' 1\" W")
+            .xy((-ui.win_w / 2.0) + 50.0, (ui.win_h / 2.0) - 445.0)
+            .font_size(16)
+            .color(self.bg_color.plain_contrast())
+            .set(LATITUDE_LABEL, ui);
+        
+        // Velocity label
+        Label::new("0.5 m/s")
+            .xy((-ui.win_w / 2.0) + 50.0, (ui.win_h / 2.0) - 465.0)
+            .font_size(16)
+            .color(self.bg_color.plain_contrast())
+            .set(VELOCITY_LABEL, ui);
+
+        // SADL slider
+        Slider::new(self.blade, -100.0, 100.0)
+            .dimensions(150.0, 30.0)
+            .xy(250.0 - (ui.win_w / 2.0), (ui.win_h / 2.0) - 520.0)
+            .rgb(0.5, 0.3, 0.6)
+            .frame(1.0)
+            .label("BLADE")
+            .label_color(white())
+            .react(|new_blade| {
+                self.try_update_blade(new_blade);
+            })
+            .set(SADL_SLIDER, ui);
+        
+        // Left status RPM
+        /*Label::new(self.l_rpm_status.as_str())
+            .xy(110.0 - (ui.win_w / 2.0), (ui.win_h / 2.0) - 60.0)
+            .font_size(32)
+            .color(self.bg_color.plain_contrast())
+            .set(L_RPM_STATUS, ui);
+        
+        // Right status RPM
+        Label::new(self.r_rpm_status.as_str())
+            .xy((ui.win_w / 2.0) - 110.0, (ui.win_h / 2.0) - 60.0)
+            .font_size(32)
+            .color(self.bg_color.plain_contrast())
+            .set(R_RPM_STATUS, ui);
+        
+        // Blade slider
+        Slider::new(self.blade, -10.0, 10.0)
+            .dimensions(200.0, 30.0)
+            .xy(110.0 - (ui.win_w / 2.0), (ui.win_h / 2.0) - 160.0)
+            .rgb(0.5, 0.3, 0.6)
+            .frame(1.0)
+            .label("Blade")
+            .label_color(white())
+            .react(|new_blade| {
+                self.try_update_blade(new_blade);
+            })
+            .set(BLADE_SLIDER, ui);
+        
+        // 12v bus label
+        Label::new("12v Bus")
+            .xy((-ui.win_w / 2.0) + 100.0, (ui.win_h / 2.0) - 215.0)
+            .font_size(32)
+            .color(self.bg_color.plain_contrast())
+            .set(VOLTAGE_12_LABEL, ui);
+        */
+
+        // Draw our UI!
+        ui.draw(c, gl);
+    }
+    
+    pub fn handle_packet(&mut self, packet: String) {
+        let packet_parts: Vec<String> = packet.split(":").map(|s| s.to_string()).collect();
+        
+        match packet_parts[0].as_str() {
+            _ => { println!("WARNING: Unknown packet ID: {}", packet_parts[0]) },
+        }
+    }
+    
+    pub fn on_key_pressed(&mut self, key: input::Key) {
+        match key {
+            _ => { },
+        }
+    }
+    
+    pub fn on_key_released(&mut self, key: input::Key) {
+        match key {
+            _ => { },
+        }
+    }
+    
+    pub fn try_update_blade(&mut self, blade: f32) -> io::Result<usize> {
+        if (blade - self.blade).abs() > 1.0 || blade == -10.0 || blade == 10.0 {
+            self.blade = blade;
+            self.send_blade()
+        } else {
+            Ok(0)
+        }
+    }
+    
+    pub fn send_blade(&self) -> io::Result<usize> {
+        let packet = format!("F{}", self.blade as i32);
+        self.socket.send_to(packet.as_bytes(), ("10.10.153.25", 30001))
+    }
+}
+
+// Widget IDs
+const LOCAL_TIME: WidgetId = 0;
+const UTC_TIME: WidgetId = LOCAL_TIME + 1;
+const MISSION_TIME_LABEL: WidgetId = UTC_TIME + 1;
+const MISSION_START_BUTTON: WidgetId = MISSION_TIME_LABEL + 1;
+const MISSION_RESET_BUTTON: WidgetId = MISSION_START_BUTTON + 1;
+const TIME_DELAY: WidgetId = MISSION_RESET_BUTTON + 1;
+const IMU_LABEL: WidgetId = TIME_DELAY + 1;
+const GPS_LABEL: WidgetId = IMU_LABEL + 1;
+const LONGITUDE_LABEL: WidgetId = GPS_LABEL + 1;
+const LATITUDE_LABEL: WidgetId = LONGITUDE_LABEL + 1;
+const VELOCITY_LABEL: WidgetId = LATITUDE_LABEL + 1;
+const L_RPM_SLIDER: WidgetId = VELOCITY_LABEL + 1;
+const R_RPM_SLIDER: WidgetId = L_RPM_SLIDER + 1;
+const STOP_BUTTON: WidgetId = R_RPM_SLIDER + 1;
+const F_PAN_SLIDER: WidgetId = STOP_BUTTON + 1;
+const F_TILT_SLIDER: WidgetId = F_PAN_SLIDER + 1;
+const SADL_SLIDER: WidgetId = F_TILT_SLIDER + 1;
+
+/*const L_RPM_STATUS: WidgetId = STOP_BUTTON + 1;
+const R_RPM_STATUS: WidgetId = L_RPM_STATUS + 1;
+const BLADE_SLIDER: WidgetId = R_RPM_STATUS + 1;
+const VOLTAGE_12_LABEL: WidgetId = BLADE_SLIDER + 1;*/
