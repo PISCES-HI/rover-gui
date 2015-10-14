cc57566f2f64571aaf7ff27a28fe51b8171adeb1
Theodore DeRego <tderego94@gmail.com>
2015-10-13 14:15:51 -1000

    Finally recording some video

--- src/video_stream.rs
+++ src/video_stream.rs
@@ -36,7 +36,7 @@ pub fn start_video_stream(path: &str, out_path: &str) -> (Texture, Arc<Mutex<Rgb
             /////////////////////////////////////////////////////
             // Open input stream
             
-            let fps: u8 = 10;
+            let fps: i64 = 10;
 
             let mut format_context = format::input(&path).unwrap();
             //format::dump(&format_context, 0, Some(path.as_str()));
@@ -55,31 +55,28 @@ pub fn start_video_stream(path: &str, out_path: &str) -> (Texture, Arc<Mutex<Rgb
                                                     Pixel::RGBA, 512, 512,
                                                     scaling::flag::BILINEAR).unwrap();
             
-            let mut input_frame = frame::Video::new(decoder.format(), decoder.width(), decoder.height());
-            let mut output_frame = frame::Video::new(Pixel::RGBA, 512, 512);
-
             /////////////////////////////////////////////////////
             // Open recording stream
 
-            let mut rec_format = ffmpeg::format::output(&format!("{}.h264", out_path)).unwrap();
+            let mut rec_format = ffmpeg::format::output(&format!("{}", out_path)).unwrap();
 
             let mut rec_video = {
-                    let mut stream = rec_format.add_stream(stream_codec.id()).unwrap();
+                    let mut stream = rec_format.add_stream(ffmpeg::codec::Id::VP9).unwrap();
                     let mut codec  = stream.codec().encoder().video().unwrap();
 
                     codec.set_width(decoder.width());
                     codec.set_height(decoder.height());
-                    //codec.set_format(ffmpeg::format::Pixel::YUV420P);
-                    codec.set_format(decoder.format());
+                    codec.set_format(ffmpeg::format::Pixel::YUV420P);
+                    //codec.set_format(decoder.format());
                     codec.set_time_base((1, fps as i32));
                     codec.set_flags(ffmpeg::codec::flag::GLOBAL_HEADER);
 
                     stream.set_time_base((1, 1_000));
                     stream.set_rate((fps as i32, 1));
 
-                    //codec.open_as(stream_codec.id()).unwrap()
+                    codec.open_as(ffmpeg::codec::Id::VP9).unwrap()
                     //codec.open().unwrap()
-                    codec.encoder()
+                    //codec.encoder()
             };
 
             let mut rec_converter =
@@ -92,13 +89,18 @@ pub fn start_video_stream(path: &str, out_path: &str) -> (Texture, Arc<Mutex<Rgb
             let mut rec_packet = ffmpeg::Packet::empty();
             let mut rec_frame  = ffmpeg::frame::Video::empty();
 
-            //let start = ffmpeg::time::relative() as i64;
-            //let sleep = 1;
+            let start = ffmpeg::time::relative() as i64;
+            let sleep = 1_000_000/fps;
 
             /////////////////////////////////////////////////////
             // Process streams
             
             for (stream, packet) in format_context.packets() {
+                let now = ffmpeg::time::relative() as i64;
+
+                let mut input_frame = frame::Video::new(decoder.format(), decoder.width(), decoder.height());
+                let mut output_frame = frame::Video::new(Pixel::RGBA, 512, 512);
+
                 decoder.decode(&packet, &mut input_frame).unwrap();
                 
                 if let Err(e) = sws_context.run(&input_frame, &mut output_frame) {
@@ -120,20 +122,20 @@ pub fn start_video_stream(path: &str, out_path: &str) -> (Texture, Arc<Mutex<Rgb
                     }
                 }
 
-                println!("converting...");
+                //println!("converting...");
 
                 // Now encode the recording packets
-                /*if let Err(e) = rec_converter.run(&input_frame, &mut rec_frame) {
+                if let Err(e) = rec_converter.run(&input_frame, &mut rec_frame) {
                     println!("WARNING: video software converter error: {}", e);
-                }*/
-                //rec_frame.set_pts(Some((now - start) / sleep));
+                }
+                rec_frame.set_pts(Some((now - start) / sleep));
 
-                println!("encoding...");
-                if rec_video.encode(&input_frame, &mut rec_packet).unwrap() {
+                //println!("encoding...");
+                if let Ok(_) = rec_video.encode(&rec_frame, &mut rec_packet) {
                     println!("Encoded!!!");
                     rec_packet.set_stream(0);
-                    //rec_packet.rescale_ts((1, fps as i32), (1, 1_000))
-                    rec_packet.write_interleaved(&mut rec_format).unwrap();
+                    rec_packet.rescale_ts((1, fps as i32), (1, 1_000));
+                    rec_packet.write_interleaved(&mut rec_format);
                 }
             }
 
